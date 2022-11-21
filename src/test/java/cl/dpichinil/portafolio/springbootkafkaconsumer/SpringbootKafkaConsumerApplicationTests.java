b94b8e7287961f1dcd2ca5131b36adf0dbf6e6e3
Daniel Pichinil <daniel.pichinil@gmail.com>
2022-11-21 16:03:38 -0300

    initial commit

---
+++ src/test/java/cl/dpichinil/portafolio/springbootkafkaconsumer/SpringbootKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.dpichinil.portafolio.springbootkafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
